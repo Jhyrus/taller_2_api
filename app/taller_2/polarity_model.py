af43db754a8bd467c0461d9c5822dcf6abac7d5e
jhyrus <diego.morales.medrano@gmail.com>
2023-05-28 19:08:01 -0500

    Cambios en servicios

--- app/taller_2/polarity_model.py
+++ app/taller_2/polarity_model.py
@@ -25,9 +25,9 @@ db = client["Taller2"]
 collection = db["Tweets"]
 
 # Conexión a la base de datos MongoDB
-#client = MongoClient('mongodb://localhost:27017')
-#db = client["taller_2"]
-#collection = db["tweets_2"]
+# client = MongoClient('mongodb://localhost:27017')
+# db = client["taller_2"]
+# collection = db["tweets_2"]
 
 # Carga el léxico de emociones desde el archivo
 emotion_lexicon = defaultdict(list)
@@ -45,31 +45,41 @@ with open('taller_2/Spanish-NRC-EmoLex.txt', 'r', encoding='utf-8') as f:
                 emotion_lexicon[emotion].append(spanish_word)
 
 # Endpoint para analizar las emociones de los tweets filtrados por usuario y rango de fechas
+
+
 @router.get("/taller_2/analyze_emotions/")
-def analyze_emotions(usuarios: str, fecha_inicio: str, fecha_fin: str):
+def analyze_emotions(usuarios: str, temas: str, fecha_inicio: str, fecha_fin: str):
     usuarios_list = usuarios.split(",")
+    temas_list = temas.split(",")
     fecha_inicio = datetime.strptime(fecha_inicio, "%d-%m-%Y")
     fecha_fin = datetime.strptime(fecha_fin, "%d-%m-%Y")
     stop_words = set(stopwords.words('spanish'))
-    processed_tweets = []
 
     query = {
         "Usuario": {"$in": usuarios_list},
+        "Tema": {"$in": temas_list},
         "Fecha": {"$gte": fecha_inicio, "$lte": fecha_fin}
     }
 
     tweets = list(collection.find(query))  # Convert cursor to a list
+
+    # Initialize data structures to store results
+    emotion_counts_per_tweet = []
+    emotion_counts_total = defaultdict(int)
+    usuario_tema_emotion_counts = defaultdict(
+        lambda: defaultdict(lambda: defaultdict(int)))
+    usuario_emotion_counts = defaultdict(lambda: defaultdict(int))
+
+    vectorizer = TfidfVectorizer()
+    processed_tweets = []
 
     for tweet in tweets:
         processed_tweet = [w.lower() for w in nltk.word_tokenize(
             tweet['Tweet']) if w not in stop_words and w.isalpha()]
         processed_tweets.append(' '.join(processed_tweet))
 
-    vectorizer = TfidfVectorizer()
     tfidf_matrix = vectorizer.fit_transform(processed_tweets)
-
     feature_names = vectorizer.get_feature_names_out()
-    emotion_counts_per_tweet = []
 
     for tweet_index, tfidf_tweet in enumerate(tfidf_matrix):
         feature_index = tfidf_tweet.nonzero()[1]
@@ -81,44 +91,79 @@ def analyze_emotions(usuarios: str, fecha_inicio: str, fecha_fin: str):
             for emotion, emotion_words in emotion_lexicon.items():
                 if word in emotion_words:
                     emotion_counts[emotion] += score
+                    emotion_counts_total[emotion] += score
+                    usuario = tweets[tweet_index]['Usuario']
+                    tema = tweets[tweet_index]['Tema']
+                    usuario_tema_emotion_counts[usuario][tema][emotion] += score
+                    usuario_emotion_counts[usuario][emotion] += score
 
         emotion_counts_per_tweet.append(dict(emotion_counts))
 
     polarity_indices_spanish = {
-        'positive': 'positivo',
-        'fear': 'miedo',
-        'negative': 'negativo',
-        'joy': 'alegría',
-        'anticipation': 'anticipación',
-        'trust': 'confianza',
-        'anger': 'ira',
-        'sadness': 'tristeza',
-        'surprise': 'sorpresa',
-        'disgust': 'asco'
+        'positive': 'Positivo',
+        'fear': 'Miedo',
+        'negative': 'Negativo',
+        'joy': 'Alegría',
+        'anticipation': 'Anticipación',
+        'trust': 'Confianza',
+        'anger': 'Ira',
+        'sadness': 'Tristeza',
+        'surprise': 'Sorpresa',
+        'disgust': 'Asco'
     }
+
+    # Convert defaultdicts to regular dicts for JSON serialization
+    usuario_tema_emotion_counts = {usuario: {tema: dict(emotion_counts) for tema, emotion_counts in tema_emotion_counts.items(
+    )} for usuario, tema_emotion_counts in usuario_tema_emotion_counts.items()}
+    usuario_emotion_counts = {usuario: dict(
+        emotion_counts) for usuario, emotion_counts in usuario_emotion_counts.items()}
+
+    # Normalize counts by the number of tweets and translate emotion names to Spanish
+    for usuario, tema_emotion_counts in usuario_tema_emotion_counts.items():
+        for tema, emotion_counts in tema_emotion_counts.items():
+            usuario_tema_emotion_counts[usuario][tema] = {polarity_indices_spanish[emotion]: round(
+                value / len(tweets), 2) for emotion, value in sorted(emotion_counts.items(), key=lambda x: x[1], reverse=True)}
+        usuario_emotion_counts[usuario] = {polarity_indices_spanish[emotion]: round(
+            value / len(tweets), 2) for emotion, value in sorted(usuario_emotion_counts[usuario].items(), key=lambda x: x[1], reverse=True)}
 
     emotion_results = []
     for tweet, emotion_counts in zip(tweets, emotion_counts_per_tweet):
         tweet["_id"] = str(tweet["_id"])  # Convert ObjectId to string
         polarity_sorted = dict(
             sorted(emotion_counts.items(), key=lambda x: x[1], reverse=True))
-        tweet["Polaridad"] = {polarity_indices_spanish[emotion]: value for emotion, value in polarity_sorted.items()}
+        tweet["Polaridad"] = {polarity_indices_spanish[emotion]: round(
+            value, 2) for emotion, value in sorted(polarity_sorted.items(), key=lambda x: x[1], reverse=True)}
         emotion_results.append(tweet)
 
-    return emotion_results
+    polarity_sorted_total = {polarity_indices_spanish[emotion]: round(
+        value / len(tweets), 2) for emotion, value in sorted(emotion_counts_total.items(), key=lambda x: x[1], reverse=True)}
+
+    return {
+        "usuario": usuarios,
+        "analisis_por_tema": usuario_tema_emotion_counts,
+        "analisis_por_usuario": usuario_emotion_counts,
+        "analisis_por_tweet": emotion_results,
+        "analisis_agregado": polarity_sorted_total
+    }
 
 # Servicio que obtiene el listado total de tweets en la base de datos
+
+
 @router.get("/taller_2/all_tweets")
 async def all_tweets() -> List:
     try:
-        documentos = await asyncio.to_thread(list, collection.find({}))  # Realiza la consulta para obtener todos los documentos
+        # Realiza la consulta para obtener todos los documentos
+        documentos = await asyncio.to_thread(list, collection.find({}))
         for doc in documentos:
-            doc['_id'] = str(doc['_id'])  # Convertir los ObjectId en cadenas de texto
+            # Convertir los ObjectId en cadenas de texto
+            doc['_id'] = str(doc['_id'])
         return documentos
     except Exception as e:
         return {"error": str(e)}
 
 # Servicio que obtiene el listado de todos los usuarios
+
+
 @router.get("/taller_2/all_users")
 async def all_users() -> List:
     try:
@@ -132,6 +177,8 @@ async def all_users() -> List:
         return {"error": str(e)}
 
 # Servicio que obtiene el listado de todos los temas
+
+
 @router.get("/taller_2/all_subjects")
 async def all_subjects() -> List:
     try:
@@ -145,6 +192,8 @@ async def all_subjects() -> List:
         return {"error": str(e)}
 
 # Servicio que obtiene un arreglo de polaridades por usuarios en un intervalo de tiempo
+
+
 @router.get("/taller_2/tweets_user_polaridad")
 async def tweets_user_polaridad(fecha_inicio, fecha_fin) -> List:
     try:
@@ -200,6 +249,8 @@ async def tweets_user_polaridad(fecha_inicio, fecha_fin) -> List:
         return {"error": str(e)}
 
 # Servicio que obtiene un arreglo de polaridades por Temas en un intervalo de tiempo
+
+
 @router.get("/taller_2/tweets_theme_polaridad")
 async def tweets_theme_polaridad(fecha_inicio, fecha_fin) -> List:
     try:
@@ -255,6 +306,8 @@ async def tweets_theme_polaridad(fecha_inicio, fecha_fin) -> List:
         return {"error": str(e)}
 
 # Servicio que obtiene un arreglo de polaridades por Ubicacion en un intervalo de tiempo
+
+
 @router.get("/taller_2/tweets_ubicacion_polaridad")
 async def tweets_ubicacion_polaridad(fecha_inicio, fecha_fin) -> List:
     try:
@@ -310,6 +363,8 @@ async def tweets_ubicacion_polaridad(fecha_inicio, fecha_fin) -> List:
         return {"error": str(e)}
 
 # Servicio que obtiene por Polaridad cuantos tweets se han publicado en un intervalo de tiempo
+
+
 @router.get("/taller_2/tweets_polaridad")
 async def tweets_polaridad(fecha_inicio, fecha_fin) -> List:
     try:
@@ -339,6 +394,8 @@ async def tweets_polaridad(fecha_inicio, fecha_fin) -> List:
         return {"error": str(e)}
 
 # Servicio que obtiene por fecha la polaridad de los tweets
+
+
 @router.get("/taller_2/tweets_fechas_polaridad")
 async def tweets_fechas_polaridad() -> List:
     try:
@@ -408,6 +465,8 @@ async def tweets_by_user(Username) -> List:
         return {"error": str(e)}
 
 # Servicio que obtiene el Tweet por Id
+
+
 @router.get("/taller_2/tweet_by_Id")
 def tweet_by_Id(Id):
     try:
@@ -419,6 +478,7 @@ def tweet_by_Id(Id):
         return tweet
     except Exception as e:
         return {"error": str(e)}
+
 
 @router.get("/taller_2/semantic_analysis_by_tweet_id")
 async def semantic_analysis_by_tweet_id(Id):
@@ -458,29 +518,39 @@ async def dbpedia_by_tweet_id(Id):
         ubicacion = tweet["Ubicación"]
         data = consultar_dbpedia_spotlight(mensaje)
         data_ubicacion = consultar_dbpedia_spotlight(ubicacion)
+
         if "Resources" in data_ubicacion:
             data_ubicacion = data_ubicacion["Resources"]
             new_data_u = []
+            surfaceForms_u = set()  # Conjunto para almacenar las surfaceForms únicas
             for item in data_ubicacion:
-                new_item = {
-                    "@surfaceForm": item["@surfaceForm"],
-                    "@URI": item["@URI"]
-                }
-                new_data_u.append(new_item)
+                surfaceForm = item["@surfaceForm"]
+                if surfaceForm not in surfaceForms_u:
+                    new_item = {
+                        "@surfaceForm": surfaceForm,
+                        "@URI": item["@URI"]
+                    }
+                    new_data_u.append(new_item)
+                    surfaceForms_u.add(surfaceForm)
         else:
             new_data_u = []
 
         if "Resources" in data:
             data = data["Resources"]
             new_data = []
+            surfaceForms = set()  # Conjunto para almacenar las surfaceForms únicas
             for item in data:
-                new_item = {
-                    "@surfaceForm": item["@surfaceForm"],
-                    "@URI": item["@URI"]
-                }
-                new_data.append(new_item)
+                surfaceForm = item["@surfaceForm"]
+                if surfaceForm not in surfaceForms:
+                    new_item = {
+                        "@surfaceForm": surfaceForm,
+                        "@URI": item["@URI"]
+                    }
+                    new_data.append(new_item)
+                    surfaceForms.add(surfaceForm)
         else:
             new_data = []
+
         respuesta = {
             "texto_original": mensaje,
             "ubicacion": ubicacion,
@@ -490,6 +560,7 @@ async def dbpedia_by_tweet_id(Id):
         return respuesta
     except Exception as e:
         return {"error": str(e)}
+
 
 @router.on_event("shutdown")
 def shutdown_event():
